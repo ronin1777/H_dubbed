1bc566a7aede3f368b43620dde48b42a6337557e
comiser <hoseincomiser27@gmail.com>
2023-12-21 20:08:06 +0330

    tdd movie model

---
+++ h_dubbed/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for h_dubbed project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'h_dubbed.settings')
+
+application = get_asgi_application()
