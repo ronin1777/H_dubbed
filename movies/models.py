72bb68cc088fbdbf5f5a77fdeeee2b663277128f
comiser <hoseincomiser27@gmail.com>
2023-12-21 20:11:57 +0330

    create model and pass test

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(_('title'), max_length=255)
+    slug = models.CharField(_('slug'), max_length=255)
+    start_release_date = models.DateField(_('start_release'), null=True, blank=True)
+    end_release_date = models.DateField(_('end_release'), null=True, blank=True)
+    description = models.TextField(_('description'), null=True, blank=True)
