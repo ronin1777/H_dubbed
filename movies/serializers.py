a4d47e097d832845ec18c510ebd32ee86908dc03
comiser <hoseincomiser27@gmail.com>
2023-12-21 22:16:34 +0330

    add MovieSerializers

---
+++ movies/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from movies.models import Movie
+
+
+class MovieSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
