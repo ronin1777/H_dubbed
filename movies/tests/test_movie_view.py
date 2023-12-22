46ea71291cc09a25557d9feccca6999aa94820a1
comiser <hoseincomiser27@gmail.com>
2023-12-22 15:54:34 +0330

    clean failed test

--- movies/tests/test_movie_view.py
+++ movies/tests/test_movie_view.py
@@ -6,14 +6,14 @@ from django.urls import reverse
 
 class MovieViewTest(APITestCase):
 
-    def setUp(self):
-        client = APIClient
-
     def test_create_movie(self):
-        data = {
+        valid_data = {
             'title': 'Test Movie',
             'slug': 'test-movie',
+            'start_release_date': '2022-01-01',
+            'end_release_date': '2022-01-31',
+            'description': 'This is a test movie'
         }
-        response = self.client.post(path='movie:cl_movie', data=data)
+        response = self.client.post('/movie/createmovie/', data=valid_data)
 
         self.assertEquals(response.status_code, 201)
