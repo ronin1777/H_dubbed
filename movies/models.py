999295614ecfeff3b2a792ca28dfdaff8d85953b
comiser <hoseincomiser27@gmail.com>
2023-12-22 17:25:32 +0330

    add cast model

--- movies/models.py
+++ movies/models.py
@@ -29,3 +29,21 @@ class Director(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Casts(models.Model):
+    GENDERS = [
+        ('m', "Male"),
+        ('f', "Female"),
+        ('o', "Other"),
+    ]
+
+    name = models.CharField(max_length=255)
+    movies = models.ForeignKey(Movie, on_delete=models.PROTECT, blank=True, null=True, related_name='cast_movies')
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDERS)
+    is_awarded = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
