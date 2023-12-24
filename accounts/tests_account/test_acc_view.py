3893b6b2d57333fc50e4bfe06d8fe4a85ba95aae
comiser <hoseincomiser27@gmail.com>
2023-12-24 21:52:35 +0330

    add some test for new view

--- accounts/tests_account/test_acc_view.py
+++ accounts/tests_account/test_acc_view.py
@@ -21,6 +21,35 @@ class TestUserRegister(APITestCase):
         self.assertEquals(User.objects.count(), 1)
 
 
+class TestUserLogin(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='hosein@gmail.com', username='hosein', password='admin123')
+        self.client = APIClient()
+
+    def test_login_valid(self):
+        response = self.client.post(reverse('users-front:user_login'), data={'username': 'hosein', 'password': 'admin1234'})
+        self.assertEquals(response.status_code, 200)
+        self.assertIn('access' and 'refresh', response.data)
+
+    def test_login_empty_data(self):
+        response = self.client.post(reverse('users-front:user_login'), data={'username': '', 'password': 'admin1234'})
+        self.assertEquals(response.data, {"username": ["This field may not be blank."]})
+        self.assertNotIn('access' or 'refresh', response.data)
+
+
+class TestUserLogout(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(email='hosein@gmail.com', username='testuser', password='testpassword')
+        self.login_response = self.client.post(reverse('users-front:user_login'), data={'username': 'testuser', 'password': 'testpassword'})
+        self.access = self.login_response.data['access']
+        self.refresh = self.login_response.data['refresh']
+        self.client.credentials(HTTP_AUTHORIZATION='Bear ' + self.access)
+
+    def test_logout(self):
+
+        logout_response = self.client.post(reverse('users-front:user_logout'))
+        self.assertEquals(logout_response.status_code, 204)
 
 
 
