849f65643fe9283ae9d8e64e5030db35e169f677
comiser <hoseincomiser27@gmail.com>
2023-12-25 17:42:19 +0330

    add post model

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,19 @@
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
 
-# Create your models here.
+from h_dubbed import settings
+from rating.models import Rating
+
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='U_posts')
+    body = models.TextField(_('body'))
+    slug = models.SlugField()
+    title = models.CharField(_('title'), max_length=100, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    rating = GenericRelation(Rating, blank=True, null=True)
+
+    class Meta:
+        ordering = ['-created']
+        
