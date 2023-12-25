bd25aeafd88fb82a1fcfd00675e995ac4b4ac7e5
comiser <hoseincomiser27@gmail.com>
2023-12-25 19:13:02 +0330

    add view

--- badge/views.py
+++ badge/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+from badge.models import Badge
+from badge.serializers import BadgeSerializer
+from movies.permisions import IsAdminOrReadOnly
+
+
+class BadgeView(ListCreateAPIView):
+    serializer_class = BadgeSerializer
+    queryset = Badge.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class BadgeUpdateView(RetrieveUpdateDestroyAPIView):
+    serializer_class = BadgeSerializer
+    queryset = Badge.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
+
