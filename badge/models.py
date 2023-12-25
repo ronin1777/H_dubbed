db7339eab4bb197b5f69ef727d8b6e4357897bd0
comiser <hoseincomiser27@gmail.com>
2023-12-25 19:02:52 +0330

    gitflow-feature-stash: badge
    add badge model

---
+++ badge/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Badge(models.Model):
+    class TypeBadgeChoice(models.TextChoices):
+        cooper = 'cooper'
+        silver = 'silver'
+        gold = 'gold'
+        diamond = 'diamond'
+    name = models.CharField(max_length=100)
+    type_badge = models.CharField(max_length=10, choices=TypeBadgeChoice.choices, default=TypeBadgeChoice.cooper)
+    description = models.TextField()
+    condition = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
--- h_dubbed/settings.py
+++ h_dubbed/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
     'rating',
     'comment',
     'reviews',
+    'badge',
 
 ]
 
