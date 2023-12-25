4822de1d135caf8313717a5c1a8760ac42f0b98d
comiser <hoseincomiser27@gmail.com>
2023-12-25 19:06:02 +0330

    register badge to admin panel

--- badge/admin.py
+++ badge/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Badge, UserBadge
 
-# Register your models here.
+
+@admin.register(Badge)
+class BadgeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'condition')
+
+
+@admin.register(UserBadge)
+class UserBadgeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'badge', 'progress')
+    
