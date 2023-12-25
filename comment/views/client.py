a4a1acbc5bbf564a90611c0fd390825212c48d25
comiser <hoseincomiser27@gmail.com>
2023-12-25 18:04:04 +0330

    add admin view

--- comment/views/client.py
+++ comment/views/client.py
@@ -1,1 +1,25 @@
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
+from comment.models import Post, Comment
+from comment.serializers import PostSerializer, CommentSerializer
+from rest_framework import filters
+
+
+class PostListViewSet(ListAPIView):
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title']
+
+
+class CommentViewSet(ListCreateAPIView):
+
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentRetrieveUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
