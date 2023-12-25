59c8ed1640bbcc59465f955cd09357a61eb4ca3b
comiser <hoseincomiser27@gmail.com>
2023-12-25 19:04:53 +0330

    add UserBadge model

--- badge/models.py
+++ badge/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from h_dubbed import settings
+
 
 class Badge(models.Model):
     class TypeBadgeChoice(models.TextChoices):
@@ -21,3 +23,11 @@ class BadgeMedia(models.Model):
     user = models.ManyToManyField(Badge, related_name='user_badge')
     media = models.ForeignKey('media.MediaModel', on_delete=models.PROTECT)
 
+
+class UserBadge(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    badge = models.ForeignKey(Badge, on_delete=models.CASCADE)
+    progress = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.user} get {self.badge}'
