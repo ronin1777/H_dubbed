bc921cf134a07c8e437831502d1b9e139085d26f
comiser <hoseincomiser27@gmail.com>
2023-12-29 17:35:15 +0330

    add QuestionInline

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 import nested_admin
 
-from quiz.models import Answer
+from quiz.models import Answer, Question
 
 
 # Register your models here.
@@ -12,3 +12,10 @@ class AnswerInline(nested_admin.NestedTabularInline):
     model = Answer
     extra = 4
     max_num = 4
+
+
+class QuestionInline(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [AnswerInline,]
+    extra = 4
+
