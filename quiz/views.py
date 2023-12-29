5612dc0524d9f3f27a34a83f9884e34c78ed2f8a
comiser <hoseincomiser27@gmail.com>
2023-12-29 17:46:56 +0330

    add QuizListAPI view

--- quiz/views.py
+++ quiz/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 
 from quiz.models import Quiz
-from quiz.serializers import QuizResultSerializers
+from quiz.serializers import QuizResultSerializers, QuizListSerializer
 
 
 # Create your views here.
@@ -16,4 +16,16 @@ class MyQuizListAPI(generics.ListAPIView):
 
     def get_queryset(self, *args, **kwargs):
         queryset = Quiz.objects.filter(quiztaker__user=self.request.user)
+        return queryset
+
+
+class QuizListAPI(generics.ListAPIView):
+    """
+    Users can see all quizzes except the ones they have answered.
+    """
+    serializer_class = QuizListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = Quiz.objects.filter(is_Active=True).exclude(quiztaker__user=self.request.user)
         return queryset
