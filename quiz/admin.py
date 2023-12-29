983f1f4c496cfd3468e44b89cb6eb8ebded3b431
comiser <hoseincomiser27@gmail.com>
2023-12-29 17:37:12 +0330

    register quiz models

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 import nested_admin
 
-from quiz.models import Answer, Question, UsersAnswer
+from quiz.models import Answer, Question, UsersAnswer, QuizTaker, Quiz, Category
 
 
 # Register your models here.
@@ -31,3 +31,12 @@ class UserAnswerInline(admin.TabularInline):
 class QuizTakerAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'quiz', 'is_passed', 'score', 'completed')
     inlines = [UserAnswerInline]
+
+
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(QuizTaker, QuizTakerAdmin)
+admin.site.register(UsersAnswer)
+admin.site.register(Category)
+
