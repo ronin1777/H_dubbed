041626ae98131b19998900eb1f57c13ad2fe2fe9
comiser <hoseincomiser27@gmail.com>
2023-12-29 17:22:24 +0330

    add quiz model

--- quiz/models.py
+++ quiz/models.py
@@ -7,3 +7,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=300)
+    cat = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    slug = models.SlugField(max_length=50, blank=True)
+    is_Active = models.BooleanField(default=True)
+    score_required = models.PositiveSmallIntegerField(default=60)
+    time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['time', ]
+        verbose_name_plural = 'Quizzes'
+
+    def __str__(self):
+        return self.title
+    
