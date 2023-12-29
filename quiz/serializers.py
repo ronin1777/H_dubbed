b2991114d20052d248ff6ddd0aab532f7ab371c5
comiser <hoseincomiser27@gmail.com>
2023-12-29 17:39:26 +0330

    add QuizListSerializer

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
-from quiz.models import Category
+from quiz.models import Category, Quiz
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['title', 'quiz_count']
+
+
+class QuizListSerializer(serializers.ModelSerializer):
+    questions_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Quiz
+        fields = ["id", "title", "slug", "questions_count"]
+        read_only_fields = ["questions_count"]
+
+    def get_questions_count(self, obj):
+        return obj.question_set.all().count()
+
