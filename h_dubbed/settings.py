a248c3d6f32532da3800c108b20d065c8a4a9a43
comiser <hoseincomiser27@gmail.com>
2023-12-31 14:39:54 +0330

    add postgresql

--- h_dubbed/settings.py
+++ h_dubbed/settings.py
@@ -123,11 +123,14 @@ WSGI_APPLICATION = 'h_dubbed.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'comiser',
+        'PASSWORD': 'Hosein67',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
